b04c73fede04d77ca0fac1cf462bbdcc59794432
Reetesh V <85978565+reeteshvriti@users.noreply.github.com>
2021-10-28 19:42:53 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,15 +2,15 @@ import smtplib
 from email.message import EmailMessage
 
 email = EmailMessage()
-email['from'] = 'rititemp2@gmail.com'
-email['to'] = 'reeteshvenki@gmail.com'
+email['from'] = 'enter sender email address'
+email['to'] = 'enter the email address of the reciver'
 email['subject'] = 'Job alert'
 
-email.set_content("Reetesh V, web developer refer this offer letter")
+email.set_content("alert for job as web developer")
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('rititemp2@gmail.com', 'Riti@9741')
+    smtp.login('sender email address', 'password of email')
     smtp.send_message(email)
     print("Email sent successfully")
 
